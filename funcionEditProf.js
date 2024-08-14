c9476a80193946c1a1cacbc4136b46f7b46522b2
Meliora <fernandezlaurabelen@hotmail.com>
2024-08-14 10:58:10 -0300

    Cambios realizados al nav

--- funcionEditProf.js
+++ funcionEditProf.js
@@ -1,5 +1,7 @@
 function toggleMenu() {
     const menu = document.getElementById('subMenu');
+    const button = document.getElementById('dropdownButton');
     menu.classList.toggle('show');
+    button.classList.toggle('active');
 }
 
