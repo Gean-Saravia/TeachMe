d09608f7db2a53b917706e88d906c51596441b02
camscript <camilaagustina2901@gmail.com>
2024-08-24 15:32:31 -0300

    Agrego función para la visibilidad de contraseñas

--- funcionlogin.js
+++ funcionlogin.js
@@ -1,3 +1,4 @@
+//Función para intercambiar formularios
 document.getElementById('toggleToSignUp').addEventListener('click', function(event) {
     event.preventDefault();
     document.getElementById('container').classList.add('active');
@@ -6,4 +7,27 @@ document.getElementById('toggleToSignUp').addEventListener('click', function(eve
 document.getElementById('toggleToSignIn').addEventListener('click', function(event) {
     event.preventDefault();
     document.getElementById('container').classList.remove('active');
+});
+
+//Función para visibilidad de contraseñas
+document.addEventListener('DOMContentLoaded', function() {
+    const togglePassword = document.querySelector('#togglePassword');
+    const toggleRepeatPassword = document.querySelector('#toggleRepeatPassword');
+    const togglePasswordLogin = document.querySelector('#togglePasswordLogin');
+    const password = document.querySelector('#password');
+    const repeatPassword = document.querySelector('#repeat-password');
+    const passwordLogin = document.querySelector('#password-login');
+
+    function togglePasswordVisibility(toggleElement, passwordElement) {
+        toggleElement.addEventListener('click', function () {
+            const type = passwordElement.getAttribute('type') === 'password' ? 'text' : 'password';
+            passwordElement.setAttribute('type', type);
+            this.classList.toggle('fa-eye-slash');
+            this.classList.toggle('fa-eye');
+        });
+    }
+
+    togglePasswordVisibility(togglePassword, password);
+    togglePasswordVisibility(toggleRepeatPassword, repeatPassword);
+    togglePasswordVisibility(togglePasswordLogin, passwordLogin);
 });
