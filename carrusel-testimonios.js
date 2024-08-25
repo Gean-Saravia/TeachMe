918d7a060f6dac801c6d24ca143d3a7b21f0a3bc
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2024-08-25 19:35:39 -0300

    se agrega el carrusel de testimonios

---
+++ carrusel-testimonios.js
@@ -0,0 +1,78 @@
+const testimonios = [
+    {
+        nombre: 'Paulo Londra',
+        curso: 'idiomas',
+        resenia: 'Realizar este curso me permitió mejorar mi comunicación y comprensión',
+        foto: './assets/cabra-icon.png'
+    },
+    {
+        nombre: 'Frank Fabra',
+        curso: 'idiomas',
+        resenia: 'Realizar este curso me permitió mejorar mi comunicación y comprensión',
+        foto: './assets/vaca-icon.png'
+    },
+    {
+        nombre: 'Mauro Lombardo',
+        curso: 'idiomas',
+        resenia: 'Realizar este curso me permitió mejorar mi comunicación y comprensión',
+        foto: './assets/pato-icon.png'
+    },
+    {
+        nombre: 'Migue Granados',
+        curso: 'idiomas',
+        resenia: 'Realizar este curso me permitió mejorar mi comunicación y comprensión',
+        foto: './assets/pez-icon.png'
+    },
+    {
+        nombre: 'Floricienta',
+        curso: 'idiomas',
+        resenia: 'Realizar este curso me permitió mejorar mi comunicación y comprensión',
+        foto: './assets/vaca-icon.png'
+    },
+    {
+        nombre: 'Equi Fernandez',
+        curso: 'idiomas',
+        resenia: 'Realizar este curso me permitió mejorar mi comunicación y comprensión',
+        foto: './assets/pato-icon.png'
+    }
+]
+
+const cont_carrusel = document.querySelector('.carrusel-tracker')
+const totalTestimonios = testimonios.length;
+let currentIndex = 0;
+const testimoniosVisibles = 4;
+
+function generarTestimonios(){
+    testimonios.forEach(testi => {
+        const card = document.createElement('div');
+        card.className = 'testimonio';
+        card.innerHTML = `
+                <img src="${testi.foto}" alt="${testi.nombre} imagen de perfil">
+                <div class="testi-estrellas">
+                    <p>&#9733;</p>
+                    <p>&#9733;</p>
+                    <p>&#9733;</p>
+                    <p>&#9733;</p>
+                    <p>&#9733;</p>
+                </div>
+                <div class="testi-detalles">
+                    <p class="testi-nombre">${testi.nombre}</p>
+                    <p class="testi-curso">Curso de ${testi.curso}</p>
+                    <p class="testi-resena">${testi.resenia}</p>
+                </div>
+            </div>
+        
+        `;
+        cont_carrusel.appendChild(card)
+    })
+}
+
+cont_carrusel.addEventListener('mouseover', () => {
+    cont_carrusel.style.animationPlayState = 'paused';
+});
+
+cont_carrusel.addEventListener('mouseout', () => {
+    cont_carrusel.style.animationPlayState = 'running';
+});
+
+generarTestimonios()
---
+++ integrantes.js
@@ -0,0 +1,67 @@
+const integrantes = [
+    {
+        nombre: "Jazmin Ferreyra",
+        rol: "Lider de Proyecto",
+        github: "https://github.com/Jazm1nne",
+        linkedin: "https://www.linkedin.com/in/jazmin-ferreyra/",
+        email: "belenjazminferreyra@gmail.com",
+        foto: "../assets/jazmin-emoji.png"
+    },
+    {
+        nombre: "Fabrizio Catanzaro",
+        rol: "Lider de Proyecto",
+        github: "https://github.com/FabrizioCatanzaro",
+        linkedin: "https://www.linkedin.com/in/luciano-fabrizio-catanzaro-pfahler/",
+        email: "fabriziocatanzaro1@gmail.com",
+        foto: "../assets/fabrizio-emoji.png"
+    },
+    {
+        nombre: "Laura Fernández",
+        rol: "Lider de Proyecto",
+        github: "https://github.com/MelioraImpera",
+        linkedin: "https://www.linkedin.com/in/fernandez-laura-belen/",
+        email: "fernandezlaurabelen@gmail.com",
+        foto: "../assets/laura-emoji.png"
+    },
+    {
+        nombre: "Gean Saravia",
+        rol: "Lider de Proyecto",
+        github: "https://github.com/Gean-Saravia",
+        linkedin: "https://www.linkedin.com/in/gean04/",
+        email: "geansaravia2004@gmail.com",
+        foto: "../assets/gean-emoji.png"
+    },
+    {
+        nombre: "Camila Cardozo",
+        rol: "Lider de Proyecto",
+        github: "https://github.com/Camscript",
+        linkedin: "https://www.linkedin.com/in/camila-cardozo-851a61253/",
+        email: "camilaagustina2901@gmail.com",
+        foto: "../assets/camila-emoji.png"
+    },
+    
+]
+
+const cont_equipo_index = document.querySelector('.cont-equipo-index');
+
+function generarCards(){
+        integrantes.forEach(integrante => {
+            const card = document.createElement('div');
+            card.className = 'equipo-index';
+            card.style.backgroundImage = `url(${integrante.foto})`;
+    
+            card.innerHTML = `
+            <p class="equipo-nombre">${integrante.nombre}</p>
+                <p class="equipo-rol">${integrante.rol}</p>
+                <div class="equipo-contacto">
+                    <a href="${integrante.github}" target="_blank"><img src="./assets/github.svg" alt="GitHub"></a>
+                    <a href="${integrante.linkedin}" target="_blank"><img src="./assets/linkedin.svg" alt="LinkedIn"></a>
+                    <a href="mailto:${integrante.email}" target="_blank"><img src="./assets/email.svg" alt="Email"></a>
+                </div>
+            `;
+    
+            cont_equipo_index.appendChild(card);
+        });
+}
+
+generarCards();
