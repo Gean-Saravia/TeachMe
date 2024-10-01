45b88ecc264864f666dbc9185cd0ed7cf75493af
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2024-09-30 22:02:14 -0300

    modificando scripts para traer los datos de la API

--- public/js/funcionInicioEstudiante.js
+++ public/js/funcionInicioEstudiante.js
@@ -1,3 +1,6 @@
+import { d, BACK_URL } from "./variables.js"
+const token = localStorage.getItem('token')
+
 function toggleMenu() {
     const menu = document.getElementById('subMenu');
     const button = document.getElementById('dropdownButton');
@@ -52,3 +55,139 @@ document.addEventListener('DOMContentLoaded', function() {
 
     calendar.render();
 });
+
+const claseProfeLista = d.querySelector('#class-list')
+const fotoCalendario = d.querySelector('.profile-info>img')
+const nombreCalendario = d.querySelector('.profile-info>h2')
+
+// Funcion para mostrar todas las clases que hayan sido creadas por el usuario (es decir, en las que es profesor)
+async function mostrarClasesProfesor(){
+    
+    try{
+        const responseToken = await fetch(`${BACK_URL}/users/token`, {
+            method: 'GET',
+            headers: {
+                'token': token
+            }
+        })
+        //console.log("response de token", response);
+        
+        if (!responseToken.ok) {
+            //alert('Su sesión expiró, por favor vuelva a logearse')
+            throw new Error(`Error: ${responseToken.status}`);
+        }
+        const data = await responseToken.json();
+        const usuario = data.user[0]
+        const idUsuario = usuario.id
+        //console.log("usuario",usuario);
+
+        const responseClases = await fetch(`${BACK_URL}/classes?id_profesor=${idUsuario}`, {
+            method: 'GET',
+        })
+        
+        if (!responseClases.ok) {
+            throw new Error(`Error: ${responseClases.status}`);
+        }
+        const dataClasesProf = await responseClases.json()
+        console.log("response de CLASES CONTRATADAS:",dataClasesProf);
+
+        if(dataClasesProf.length > 0){
+            dataClasesProf.forEach(clase => {
+                
+                const claseListada = d.createElement('li');
+                claseListada.className = 'item';
+                claseListada.innerText = `${clase.titulo}`;
+
+                claseListada.addEventListener('click', () => highlightItem(this))
+
+                claseProfeLista.appendChild(claseListada)
+                
+            })
+        } else {
+            console.log("No hay clases contratadas!");
+        }
+        nombreCalendario.innerText = `${usuario.nombre}`
+        fotoCalendario.src = usuario.foto_perfil ==  null ? "/assets/Icon-user.png" : `${usuario.foto_perfil}`
+        fotoCalendario.alt = `${usuario.nombre}`
+        
+    } catch(error){
+        console.error('Hubo un problema con la petición:', error);
+    }
+}
+
+const claseAlumnoLista = d.querySelector('.subjects')
+const tablaAlumnoLista = d.querySelector('.class-table>tbody')
+
+async function mostrarClasesEstudiante(){
+    try{
+        const responseToken = await fetch(`${BACK_URL}/users/token`, {
+            method: 'GET',
+            headers: {
+                'token': token
+            }
+        })
+        //console.log("response de token", response);
+        
+        if (!responseToken.ok) {
+            //alert('Su sesión expiró, por favor vuelva a logearse')
+            throw new Error(`Error: ${responseToken.status}`);
+        }
+        const data = await responseToken.json();
+        const usuario = data.user[0]
+        const idUsuario = usuario.id
+        //console.log("usuario",usuario);
+
+        const responseClases = await fetch(`${BACK_URL}/hirings?id_alumno=${idUsuario}`, {
+            method: 'GET',
+        })
+        
+        if (!responseClases.ok) {
+            throw new Error(`Error: ${responseClases.status}`);
+        }
+        const dataClasesAlum = await responseClases.json()
+        console.log("response de CLASES ASISTIDAS:",dataClasesAlum);
+
+        if(dataClasesAlum.length > 0){
+            dataClasesAlum.forEach(clase => {
+                
+                const claseListada = d.createElement('div');
+                claseListada.className = 'subject';
+                claseListada.innerHTML = `
+                    <h3>${clase.clase}</h3>
+                    <span class="profile-icon">👨‍💼</span>
+                    <p>Docente: ${clase.profesor}</p>
+                `;
+                claseAlumnoLista.appendChild(claseListada)                
+                
+                const tablaListada = d.createElement('tr')
+                
+                const fecha = new Date(clase.fecha_contratacion);
+                // Obtener las partes de la fecha y formatearlas
+                const dia = fecha.getDate().toString().padStart(2, '0'); // Aseguramos dos dígitos para el día
+                const mes = (fecha.getMonth() + 1).toString().padStart(2, '0'); // Sumamos 1 al mes porque enero es 0
+                const año = fecha.getFullYear();
+
+                // Crear la cadena formateada
+                const fechaFormateada = `${dia}/${mes}/${año}`;
+
+                //console.log("fecha:", fechaFormateada);
+                
+                tablaListada.innerHTML = `
+                    <td>${clase.clase}</td>
+                    <td>${clase.profesor} ${clase.profe_apellido}</td>
+                    <td>$${clase.total.toLocaleString('es-ES')}</td>
+                    <td>${fechaFormateada}</td>
+                `;
+                tablaAlumnoLista.appendChild(tablaListada)
+
+            })
+        } else {
+            console.log("No hay clases contratadas!");
+        }
+        
+    } catch(error){
+        console.error('Hubo un problema con la petición:', error);
+    }
+}
+mostrarClasesProfesor()
+mostrarClasesEstudiante()
--- public/js/tarjetas-grandes.js
+++ public/js/tarjetas-grandes.js
@@ -1,3 +1,5 @@
+import { d, BACK_URL } from "./variables.js"
+
 //Función del navbar
 function toggleMenu() {
     const menu = document.getElementById('subMenu');
@@ -44,9 +46,82 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+const urlParams = new URLSearchParams(window.location.search);
+console.log("params:",urlParams);
+
+const cursoId = urlParams.get('curso');
+console.log("cursoId:",cursoId);
+
+const sectionClases = d.querySelector('.cards-conteiner')
+const searchBar = d.querySelector('#search-bar')
+
+let data = []
+
+async function generarProfes(busqueda){
+    let filtrado = ''
+    if(!busqueda){
+        filtrado = ''
+    } else{
+        filtrado = busqueda
+    }
+    try{
+        const response = await fetch(`${BACK_URL}/classes?id_materia=${cursoId}&titulo=${filtrado}`, {
+            method: 'GET',
+        })
+        console.log("response de TARJ GRANDES:",response);
+        
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status}`);
+        }
+        data = await response.json();
+        console.log("DATA DE TARJ GRANDES:",data);
+
+        sectionClases.innerHTML = '';
+        if(data.length > 0){
+            data.forEach(clase => {
+                
+                const card = d.createElement('article');
+                card.className = 'card';
+                card.innerHTML = `
+                <div class="image-card">
+                    <img src="${clase.foto_profe}" alt="${clase.profesor}">
+                </div>
+                <div class="card-content">
+                    <h3>${clase.profesor}</h3>
+                    <button class="btn-py">${clase.titulo}</button>
+                    <div class="rating">★★★★★</div>
+                    <button data-clase-id=${clase.id} class="btn-c">Contratar</button>
+                </div>
+                `;
+                sectionClases.appendChild(card);
+
+                card.querySelector('.btn-c').addEventListener('click', (event) => {
+                    console.log(event);
+                    
+                    const claseId = event.target.dataset.claseId;
+                    window.location.href = `prof-contratar.html?clase=${claseId}`;
+                });
+            })
+        } else {
+            sectionClases.innerHTML = `<p class="busqueda-sin-exito">No se encontraron resultados, intente con otro término!</p>`
+        }
+        
+    } catch(error){
+        console.error('Hubo un problema con la petición:', error);
+    }
+}
+
+searchBar.addEventListener('input', () => {
+    const query = searchBar.value.toLowerCase();
+    //const resultados = data.filter(curso => curso.nombre_materia.toLowerCase().includes(query));
+    console.log("query: ",query);
+    generarProfes(query);
+
+})
+
 // Ejecutar las funciones cuando el DOM esté completamente cargado
 document.addEventListener('DOMContentLoaded', () => {
-    renderTeacherCards();
-    setupSearchFilter();
+    generarProfes();
+    //setupSearchFilter();
 });
 
